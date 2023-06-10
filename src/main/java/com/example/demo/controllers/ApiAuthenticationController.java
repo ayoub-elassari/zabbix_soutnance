95e67df8a1e0f5fd8bfd8787dc2859f2aca1b93c
ayoub el assari <elassari.ayoub0@gmail.com>
2023-06-10 12:22:24 +0100

    first commit

--- src/main/java/com/example/demo/controllers/ApiAuthenticationController.java
+++ src/main/java/com/example/demo/controllers/ApiAuthenticationController.java
@@ -5,33 +5,25 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
-public class Api {
+public class ApiAuthentication {
 
     private final RestTemplate restTemplate;
 
     @Autowired
-    public Api(RestTemplate restTemplate) {
+    public ApiAuthentication(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @RequestMapping(value = "/auth")
     @ResponseBody
     public String getToken() {
-        String apiUrl = "http://192.168.23.132/zabbix/api_jsonrpc.php";
+        String apiUrl = "http://192.168.23.133/zabbix/api_jsonrpc.php";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
